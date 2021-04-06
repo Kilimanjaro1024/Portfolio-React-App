52b0b3bad7af43397bdc821998a6f0196dfa972f
Clayton Miller <clayton.miller@loop.colum.edu>
2021-04-06 17:20:15 -0600

    Creates Skills.scss to style icons imported from react-icons library. Adds base content to Skills section.

--- src/components/body-components/Skills.js
+++ src/components/body-components/Skills.js
@@ -1,11 +1,33 @@
 import React from "react"
+import "../../styling/Skills.scss"
+import { FaCss3, FaHtml5, FaUnity, FontAwesome } from "react-icons/fa"
+import { DevIcons, DiCodepen, DiGit, DiGithub, DiHeroku, DiJavascript, DiJqueryLogo, DiLinux, DiMongodb, DiMysql, DiNodejs, DiPostgresql, DiReact, DiRuby, DiSass, DiTrello, DiUbuntu } from "react-icons/di"
 
 const Skills = (props) =>{
     return (
         <div>
             <h1>Skills</h1>
             <div>
-                
+                <FaHtml5 />
+                <FaUnity/>
+                <FaCss3/>
+                <DiCodepen/>
+                <DiJavascript/>
+                <DiGit/>
+                <DiGithub/>
+                <DiHeroku/>
+                <DiLinux/>
+                <DiJqueryLogo/>
+                <DiPostgresql/>
+                <DiNodejs/>
+                <DiMongodb/>
+                <DiMysql/>
+                <DiReact/>
+                <DiSass/>
+                <DiUbuntu/>
+                <DiRuby/>
+                <DiTrello/>
+
             </div>
         </div>
     )
