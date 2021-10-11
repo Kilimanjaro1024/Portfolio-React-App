0dd1311e542ef9bdf9828c163b3765a62546474c
Clayton <clayton.miller@loop.colum.edu>
2021-10-11 18:02:54 -0500

    Adds new favicon and links for footer.

--- src/components/page-components/Footer.js
+++ src/components/page-components/Footer.js
@@ -1,12 +1,17 @@
 import React from "react"
 import "../../styling/Footer.scss"
 import {FaGithub, FaLinkedin, FontAwesome} from "react-icons/fa"
+import { Link } from "react-router-dom"
 
 const Footer = (props) =>{
     return (
         <footer>
-            <FaLinkedin/>
-            <FaGithub/>
+            <a href="https://www.linkedin.com/in/clayton-miller-software-engineer/">
+                <FaLinkedin />
+            </a>
+            <a href="https://github.com/Kilimanjaro1024">
+                <FaGithub />
+            </a>
         </footer>
     )
 
